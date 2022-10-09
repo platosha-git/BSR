5d22239c1baa9ea474542cd0bbe1822d1d42169e
platosha-git <15platosha@gmail.com>
2022-10-10 02:03:06 +0300

    fixed func

---
+++ lab1/area.py
@@ -0,0 +1,9 @@
+class Area:
+    def __init__(self, X, Y, Z, x0, y0, cX, cY):
+        self.x = X
+        self.j = j
+        self.isAlive = True
+        self.u = np.nan
+
+        self.A = 0
+        self.n = 0
--- lab1/main.py
+++ lab1/main.py
@@ -21,6 +21,12 @@ def rightPart(x, y, x0, y0):
     return f(x, y, x0, y0) / k
 
 
+# краевое 2ого рода
+# -k du/dx = F0
+
+# краевое 3его рода
+# -k du/dx = a1 (u - u0)
+
 F0 = 30
 a0 = 0.1
 def CheckBound(p: Particle, X, Y, x0, y0, cX, cY, indC, indD, u0):
@@ -96,42 +102,35 @@ def main():
     form_circuit(Z, u0)
     
     cX = (a - c) / 2
-    cY = (b - d) / 2 
+    cY = (b - d) / 2
 
-    x0 = a / 2 
-    # y0 = b / 2
+    hX = X[1] - X[0]
+    hY = Y[1] - Y[0]
+    hS = hX * hY
+
+    icX = int(cX / hX)
+    jcY = int(cY / hY) 
     
-    # x0 = cX / 2
+    x0 = a / 2
     y0 = cY / 2
 
     indC = int((lenX / a) * c)
     indD = int((lenY / b) * d)
 
-    #print("x=", X)
-    #print("y=", Y)
-    #print("z=", Z)
+    for i in range(icX, icX + indC + 1):
+        for j in range(jcY, jcY + indD):
+            Z[i][j] = np.nan
 
-    #print("cX = ", cX)
-    #print("cY = ", cY)
+    for j in range(jcY, jcY + indD + 1):
+        Z[icX][j] = u0
+        Z[icX + indC][j] = u0
 
-    #hS = hX * hY
-
-    #print("hX = ", hX)
-    #print("hY = ", hY)
-
-    #print("icX = ", icX)
-    #print("jcY = ", jcY)
-
-    #print(cY, cY + d)
-    #print(Y[jShiftY], Y[jShiftY + indD])
-    #print()
-
-    #print(shiftX, shiftX + c)
-    #print(X[iShiftX], X[iShiftX + indC])
-    #print()
+    for i in range(icX, icX + indC + 1):
+        Z[i][jcY] = u0
+        Z[i][jcY + indD] = u0
 
     # run stochastic method
-    M = 10
+    M = 100
     t = time()
     for i in range(lenX):
         for j in range(lenY):
@@ -186,55 +185,4 @@ def release_shared(name):
     shm.unlink()  # Free and release the shared memory block
 
 # Z = np.full((len(X), len(Y)), -1, dtype=np.double)
-
-
-
-for i in range(iShiftX, iShiftX + indC + 1):
-    for j in range(jShiftY, jShiftY + indD):
-        Z[i][j] = np.nan
-
-for j in range(jShiftY, jShiftY + indD + 1):
-    Z[iShiftX][j] = u0
-    Z[iShiftX + indC][j] = u0
-
-for i in range(iShiftX, iShiftX + indC + 1):
-    Z[i][jShiftY] = u0
-    Z[i][jShiftY + indD] = u0
-
-
-# краевое 2ого рода
-# -k du/dx = F0
-
-# краевое 3его рода
-# -k du/dx = a1 (u - u0)
-
-
-
-
-# t = time()
-
-# res = Parallel(n_jobs=4)(delayed(stochastic_solve_point)(i, j, X, Y, Z, IsBound, rightPart, M) for j in range(lenY) for i in range(lenX))
-# Z = np.asarray(res).reshape(lenX, lenY)
-
-# print(f"par stochastic method: {time() - t} sec")
-
-zmax = Z[0][0]
-imax = 0
-jmax = 0
-for i in range(len(X)):
-    for j in range(len(Y)):
-        if zmax < Z[i][j]:
-            zmax = Z[i][j]
-            imax = i
-            jmax = j
-
-print(x0, y0)
-print(X[imax], Y[jmax])
-print("max Z = ", zmax)
-test = (X[imax], Y[jmax], zmax)
-np.set_printoptions(precision=0)
-# print(Z)
-
-X, Y = np.meshgrid(X, Y, indexing='ij')
-show_3d_graph(X, Y, Z, center=(x0, y0)) # test=test
 '''
